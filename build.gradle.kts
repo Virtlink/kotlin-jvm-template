6e0d9e9d963cfc5d678771af6241525194b64790
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-01 00:06:27 +0200

    Add signing plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import com.adarshr.gradle.testlogger.theme.ThemeType
 plugins {
     `java-library`
     `maven-publish`
+    signing
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.gitVersion)          // Set gitVersion() from last Git repository tag
     alias(libs.plugins.benmanesVersions)    // Check for dependency updates
