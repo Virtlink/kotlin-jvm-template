737f3b3203be97294f41aae932ec5bbccfbd83ae
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2026-07-29 16:31:16 +0200

    Add Nix development environment

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          jdk = pkgs.jdk21;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              jdk
+            ];
+
+            JAVA_HOME = jdk.home;
+          };
+        });
+    };
+}
